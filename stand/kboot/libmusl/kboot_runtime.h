da21b452d0925b4a86228c78a25c3af0e960c0f3
Aryan Arora <aryanarora.w1@gmail.com>
2026-07-04 23:59:11 +0530

    refactor musl resolver configuration

--- stand/kboot/libmusl/kboot_runtime.h
+++ stand/kboot/libmusl/kboot_runtime.h
@@ -6,4 +6,5 @@ extern int errno;
 
 void *Calloc(size_t, size_t, const char *, int);
 void Free(void *, const char *, int);
+_Bool file2str(const char *, char *, size_t);
 long host_syscall(int, ...);
